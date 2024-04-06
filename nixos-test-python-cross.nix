f799ce6ccd294a5f3abb4013fcc958abe79e585a
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2024-04-05 22:25:38 -0300

    Tests: nixos-test-python-cross.nix

--- nixos-test-python-cross.nix
+++ nixos-test-python-cross.nix
@@ -6,68 +6,65 @@
       name = "nixos-test-python-cross";
       nodes = {
         machineA = { config, pkgs, ... }: {
-          environment.systemPackages = with pkgs; [
-            file
-            pkgsCross.aarch64-multiplatform.python3
+          environment.systemPackages = [
+            pkgs.file
+            (pkgs.python3.withPackages
+              (pyPkgs: with pyPkgs; [ numpy ])
+            )
           ];
         };
 
         machineB = { config, pkgs, ... }: {
           boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
           environment.systemPackages = [
-            pkgs.pkgsCross.aarch64-multiplatform.python3
-          ];
-        };
-
-        machineC = { config, pkgs, ... }: {
-          boot.binfmt.registrations = {
-            s390x-linux = {
-              # interpreter = getEmulator "s390x-linux";
-              interpreter = "${pkgs.qemu}/bin/qemu-s390x";
-              magicOrExtension = ''\x7fELF\x02\x02\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x16'';
-              mask = ''\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff'';
-            };
-          };
-          environment.systemPackages = [
-            pkgs.pkgsCross.s390x.python3
+            (pkgs.pkgsCross.aarch64-multiplatform.python3.withPackages
+              (pyPkgs: with pyPkgs; [ numpy ])
+            )
           ];
         };
 
         machineD = { config, pkgs, ... }: {
           boot.binfmt.emulatedSystems = [ "riscv64-linux" ];
           environment.systemPackages = [
-            pkgs.pkgsCross.riscv64.python3
+            (pkgs.pkgsCross.riscv64.python3.withPackages
+              (pyPkgs: with pyPkgs; [ numpy ])
+            )
           ];
         };
 
         machineE = { config, pkgs, ... }: {
           boot.binfmt.emulatedSystems = [ "armv7l-linux" ];
           environment.systemPackages = [
-            pkgs.pkgsCross.armv7l-hf-multiplatform.python3
+            (pkgs.pkgsCross.armv7l-hf-multiplatform.python3.withPackages
+              (pyPkgs: with pyPkgs; [ numpy ])
+            )
           ];
         };
 
         machineF = { config, pkgs, ... }: {
           boot.binfmt.emulatedSystems = [ "armv6l-linux" ];
           environment.systemPackages = [
-            pkgs.pkgsCross.raspberryPi.python3
+            pkgs.pkgsCross.raspberryPi.pkgsStatic.python3
+            (pkgs.pkgsCross.raspberryPi.python3.withPackages
+              (pyPkgs: with pyPkgs; [ numpy ])
+            )
           ];
         };
-
       };
 
-/*
-        machineA.succeed("file $(readlink -f $(which python3)) | grep -q -F -e ': ELF 64-bit LSB executable, ARM aarch64, version 1 (SYSV), statically linked, not stripped'")
-        machineA.succeed("! ldd $(readlink -f $(which python3)) | grep -q -F -e 'not a dynamic executable'")
-        machineA.succeed("! patchelf --print-needed $(readlink -f $(which python3)) | grep -q -F -e 'patchelf: cannot find section '.dynamic'. The input file is most likely statically linked'")
-        machineA.succeed("! readelf --dynamic $(readlink -f $(which python3)) | grep -q -F -e 'There  is no dynamic section in this file.'")
-*/
-      testScript = ''
-        machineA.succeed("! python3 | grep -q -F -e ' exec format error: '")
-        machineB.succeed("python3 --version | grep -q -F -e '3.11.8'")
-        machineC.succeed("python3 --version | grep -q -F -e '3.11.8'")
-        machineD.succeed("python3 --version | grep -q -F -e '3.11.8'")
-        machineE.succeed("python3 --version | grep -q -F -e '3.11.8'")
-        machineF.succeed("python3 --version | grep -q -F -e '3.11.8'")
+      testScript = let
+        testCommand = ''
+          python3 \
+          -c \
+          'import numpy as np;
+          np.array_equal(np.array([1,2]), np.sqrt(np.square(np.array([1,2]))))
+          '
+        '';
+      in ''
+        machineA.succeed("${testCommand}")
+        machineB.succeed("${testCommand}")
+        machineD.succeed("${testCommand}")
+        machineE.succeed("${testCommand}")
+        machineF.succeed("${testCommand}")
       '';
     })
